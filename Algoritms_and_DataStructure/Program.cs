14ece725c2a3eb233caf58372ce94ded555bbf14
NyIIIa <nedviga.ivan2003@gmail.com>
2022-04-15 15:50:08 +0300

    innit

---
+++ Algoritms_and_DataStructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algoritms_and_DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
